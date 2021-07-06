f5a2ecf86017c66c5dbb453849fd08af897130fc
pratik1596 <pjk37@njit.edu>
2021-07-06 01:57:41 -0400

    Feature: Created addition function Task: Dockerfile updated Task: Added Unit Test Addition.csv Task: Created CSVReader.py

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,17 @@
+#Static Methods
+
+def addition(value1, value2):
+    value1 = int(value1)
+    value2 = int(value2)
+    return value1 + value2
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self, value1, value2):
+        self.result = addition(value1, value2)
+        return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,11 +1,24 @@
 import unittest
 from Calculator import Calculator
+from CSVReader import CSVReader
 
 class MyTestCase(unittest.TestCase):
-    def test_instaniate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(Calculator, Calculator)
 
+    def setUp(self) -> None:
+        self.object = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.object, Calculator)
+
+    def test_addition_method_calculator(self):
+        print("Testing Addition")
+        test_data = CSVReader('./src/Unit Test Addition.csv').data
+        for row in test_data:
+            a = row['Value 1']
+            b = row['Value 2']
+            real_result = int(row['Result'])
+            self.assertEqual(self.object.add(a, b), real_result)
+            self.assertEqual(self.object.result, real_result)
 
 if __name__ == '__main__':
     unittest.main()
